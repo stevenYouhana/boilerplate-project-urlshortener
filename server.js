35ac623678c7269464e47181c395f75b91184046
Steven Youhana <stevenyouhana@hotmail.co.nz>
2019-01-20 08:54:42 +1300

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const validURL = require('valid-url');
+const dns = require('dns');
 var cors = require('cors');
 
 var app = express();
@@ -11,27 +13,56 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
-
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function(req, res){
+const schema = mongoose.Schema({url: {type: String}});
+const urlmodel = mongoose.model('urlmodel', schema);
+
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+app.get('/new/*', (req, res) => {
+  console.log("GET");
+  console.log(req.params[0])
+    urlmodel.findById(req.params[0], (err, data) => {
+      if (err) console.log("err findById")
+      if (data) {
+        console.log("redirecting...");
+        console.log(data.url);
+        res.redirect(data.url);
+      }
+      else {
+        res.send("short url not found")  
+      }
+    })
+})
 
+app.post('/api/shorturl/new', (req, res) => {
+  console.log('POST')
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+  if (!validURL.isUri(req.body.url)) res.send({err: "invalid url"})
+  // urlmodel.deleteMany((err,data) => {console.log("remove all")});
+  urlmodel.findOne({url: req.body.url}, (err, data) => {
+    if (err) console.log("error posting");
+    if (data) {
+      console.log("finding");
+      console.log(data);
+      res.send({original_url:data.url, short_url: data._id})
+    }
+    else {
+      console.log("not finding");
+      var newUrl = new urlmodel({url: req.body.url});
+      newUrl.save((err, data) => {
+        if (err) throw err;
+        console.log("saving "+data)
+      })
+    }
+  });
 
-
+})
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
